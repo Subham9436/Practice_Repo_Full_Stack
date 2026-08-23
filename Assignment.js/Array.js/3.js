b8f2c63e38a252a0a00056e5158dfacb876600d5
Saanvi Shreshth <saanvishreshth@gmail.com>
2026-08-23 19:19:37 +0530

    feat(Assignment):assessment

---
+++ Assignment.js/Array.js/3.js
@@ -0,0 +1,13 @@
+// remove duplicates
+
+let arr = [1, 1, 2, 2, 3, 4, 4];
+let j = 0;
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[j]) {
+        j++;
+        arr[j] = arr[i];
+    }
+}
+
+console.log(arr.slice(0, j + 1));
